4953e3a26a2813b41f71fa6e2f1c16a5ca28533f
KRom-i <emailroma@mail.ru>
2021-08-28 04:00:45 +0300

    HW_4

---
+++ src/main/java/HomeWork_4/java/com/example/spring_boot_product/ProductService.java
@@ -0,0 +1,35 @@
+package HomeWork_4.java.com.example.spring_boot_product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> getProducts(){
+        return repository.getProducts();
+    }
+
+    public void addProduct(Product product){
+
+        product.setId(repository.getProducts().size() + 1);
+
+        repository.addProduct(product);
+    }
+
+    public Product getProductById (int id) {
+
+        if (id <= 0 || id > repository.getProducts().size()) return null;
+
+        return repository.getProducts().get(id - 1);
+    }
+
+    public Product getProduct () {
+        return new Product();
+    }
+}
